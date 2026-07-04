47c651c3781ec1f6c07b28530798c7182bebaf0b
xiaoyi51 <182205408+xiaoyi51@users.noreply.github.com>
2026-07-04 18:18:07 +0800

    updated project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
